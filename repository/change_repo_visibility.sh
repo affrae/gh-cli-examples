3f85a075e3ef2d40d2917a1909dbd328169abe6c
Daniel Figucio <745341+affrae@users.noreply.github.com>
2023-04-21 17:01:33 +1000

    Update change_repo_visibility.sh

--- repository/change_repo_visibility.sh
+++ repository/change_repo_visibility.sh
@@ -20,4 +20,4 @@ gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /repos/$OWNER/$REPO \
-  -f visibility="$VISIBILITY"
+  -fvisibility=$VISIBILITY
