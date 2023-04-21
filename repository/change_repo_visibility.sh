190572132163a85d8370ebd3c1c257bebb632809
Daniel Figucio <745341+affrae@users.noreply.github.com>
2023-04-21 16:32:18 +1000

    Create change_repo_visibility.sh

---
+++ repository/change_repo_visibility.sh
@@ -0,0 +1,8 @@
+!# /bin/bash
+
+gh api \
+  --method PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/OWNER/REPO \
+  -f visibility='internal'
