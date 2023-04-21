0bd1b6f2ad376945220a8a681aaed6e8923cd9cc
Daniel Figucio <745341+affrae@users.noreply.github.com>
2023-04-21 16:39:02 +1000

    Update change_repo_visibility.sh

--- repository/change_repo_visibility.sh
+++ repository/change_repo_visibility.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
 
+# Get the options
+while getopts ":h:o:r" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      o)
+          OWNER=${OPTARG};;
+      r)
+          REPO=${OPTARG};;   esac
+done
+
 gh api \
   --method PATCH \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/OWNER/REPO \
+  /repos/$OWNER/$REPO \
   -f visibility='internal'
