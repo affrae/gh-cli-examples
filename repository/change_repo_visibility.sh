dad8a5ba67a78d0f2067cb39aaf235b24bf9ce0e
Daniel Figucio <745341+affrae@users.noreply.github.com>
2023-04-21 17:15:49 +1000

    Update change_repo_visibility.sh

--- repository/change_repo_visibility.sh
+++ repository/change_repo_visibility.sh
@@ -19,9 +19,25 @@ echo "OWNER=$OWNER"
 echo "REPO=$REPO"
 echo "VISIBILITY=$VISIBILITY"
 
-gh api \
+# Change the repository visibility to $VISIBILITY
+gh api --silent \
   --method PATCH \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/$OWNER/$REPO \
+  /repos/"$OWNER"/"$REPO" \
   -f visibility=$VISIBILITY
+  
+# Get the repository details to determine its visibility
+RESULT=$(gh api repos/"$OWNER"/"$REPO")
+
+# Parse the result to show the owner, repository name, and visibility
+OWNER=$(echo "$RESULT" | jq -r '.owner.login')
+REPO=$(echo "$RESULT" | jq -r '.name')
+VISIBILITY=$(echo "$RESULT" | jq -r '.visibility')
+
+echo "New Values:"
+echo "   Owner: $OWNER"
+echo "   Repository: $REPO"
+echo "   Visibility: $VISIBILITY"  
+echo "Done."
+  
